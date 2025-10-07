5d5f5b1aa174b19c90357aca069656ce2868cb39
evanwiseman <evanwiseman2000@gmail.com>
2025-10-07 10:28:06 -0700

    fixed delete device route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,7 +5,7 @@ import "net/http"
 // Local LAN routes
 func LocalRoutes(s *LocalServer, mux *http.ServeMux) {
 	mux.HandleFunc("POST /devices", s.PostDeviceHandler)
-	mux.HandleFunc("DELETE /device/{deviceID}", s.DeleteDeviceByIDHandler)
+	mux.HandleFunc("DELETE /devices/{deviceID}", s.DeleteDeviceByIDHandler)
 }
 
 // Public internet routes
