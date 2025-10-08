209f0f2f4eea56c779850c39164e3995ef7e0d40
evanwiseman <evanwiseman2000@gmail.com>
2025-10-08 12:33:58 -0700

    added wdlm routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,12 +6,17 @@ import "net/http"
 func LocalRoutes(s *LocalServer, mux *http.ServeMux) {
 	// Device Endpoints
 	mux.HandleFunc("POST /devices", s.PostDeviceHandler)
+	mux.HandleFunc("PUT /devices", s.PutDevicesHandler)
 	mux.HandleFunc("GET /devices", s.GetDevicesHandler)
 	mux.HandleFunc("GET /devices/{deviceID}", s.GetDeviceByIDHandler)
-	mux.HandleFunc("PUT /devices", s.PutDevicesHandler)
 	mux.HandleFunc("DELETE /devices/{deviceID}", s.DeleteDeviceByIDHandler)
 
 	// WDLM Endpoints
+	mux.HandleFunc("POST /wdlms", s.PostWdlmHandler)
+	mux.HandleFunc("PUT /wdlms", s.PutWdlmHandler)
+	mux.HandleFunc("GET /wdlms", s.GetWdlmsHandler)
+	mux.HandleFunc("GET /wdlms/{wdlmID}", s.GetWdlmByIDHandler)
+	mux.HandleFunc("DELETE /wdlms/{wdlmID}", s.DeleteWdlmByID)
 
 	// Admin Endpoints
 	mux.HandleFunc("POST /admin/reset/devices", s.ResetDevicesHandler)
